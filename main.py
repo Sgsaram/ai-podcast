0c0da5a73feb3ef0d2330e3fc1dc45974364ceab
john <john>
2024-07-05 17:49:44 +0300

    Finished web fimally

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import random
+
+import fastapi
+import fastapi.staticfiles
+import fastapi.responses
+import fastapi.templating
+
+import pydantic
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__));
+TEST_VIEWS = random.randint(1, 200)
+TEST_RECOMMENDATION = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
+
+
+class PodcastDesc(pydantic.BaseModel):
+    description: str
+    link: str | None
+
+
+class SummationResponse(pydantic.BaseModel):
+    views: int
+    recommendation: str
+
+
+app = fastapi.FastAPI()
+
+app.mount("/static", fastapi.staticfiles.StaticFiles(directory="static"), name="static")
+templates = fastapi.templating.Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=fastapi.responses.HTMLResponse)
+async def read_root(request: fastapi.Request):
+    return templates.TemplateResponse(name="index.html", request=request)
+
+
+@app.post("/process/", response_model=SummationResponse)
+async def get_summation(podcast_desc: PodcastDesc):
+    if len(podcast_desc.description) == 0:
+        return fastapi.HTTPException(400, "No description provided")
+    response = SummationResponse(
+        views = TEST_VIEWS,
+        recommendation=TEST_RECOMMENDATION,
+    )
+    return response
+
---
+++ static/script.js
@@ -0,0 +1,132 @@
+const button_get = document.getElementById("button-get");
+const button_clear = document.getElementById("button-clear");
+const output = document.getElementById("output");
+const loading = document.getElementById("loading");
+const description = document.getElementById("description");
+const link = document.getElementById("link");
+const required = document.getElementById("required");
+const views = document.getElementById("views");
+const recommendations = document.getElementById("recommendations");
+
+const api_url = "http://127.0.0.1:8000/process"
+
+let bad_desc = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function handle_button_get() {
+    const input_text = description.value;
+    if (input_text.trim().length == 0) {
+        description.classList.add("border-rose-500");
+        description.classList.remove("border-blue-300");
+        required.classList.remove("hidden");
+        bad_desc = true;
+        return;
+    }
+    const link_text = link.value;
+
+    output.classList.add("hidden");
+    loading.classList.add("flex");
+    loading.classList.remove("hidden");
+    button_get.classList.add("hidden");
+
+    const post_data = {
+        description: input_text,
+        link: link_text,
+    };
+
+    views.textContent = -179;
+    recommendations.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis sapien elit, sed elementum nisl rhoncus congue. Quisque a vestibulum justo, ac sollicitudin mi. In viverra aliquam ex viverra ullamcorper. Suspendisse mauris ligula, malesuada in arcu vitae, ultrices pharetra purus. Duis posuere dolor eu ante rutrum tincidunt. Integer cursus, ex.";
+
+    try {
+        const response = await fetch(api_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(post_data),
+        })
+        const result = await response.json();
+        views.textContent = result.views;
+        recommendations.textContent = result.recommendation;
+    } catch(error) {
+        console.error("Error:", error);
+        console.log("Reload the page");
+    }
+
+    loading.classList.add("hidden");
+    loading.classList.remove("flex");
+    output.classList.remove("hidden");
+}
+
+button_get.addEventListener("click", handle_button_get);
+button_clear.addEventListener(
+    "click",
+    () => {
+        output.classList.add("hidden");
+        button_get.classList.remove("hidden")
+        description.value = "";
+    }
+);
+description.addEventListener(
+    "click",
+    () => {
+        if (bad_desc) {
+            required.classList.add("hidden");
+            description.classList.add("border-blue-300");
+            description.classList.remove("border-rose-500");
+            bad_desc = false;
+        }
+    }
+)
+
+button_get.addEventListener(
+    "mousedown",
+    () => {
+        button_get.classList.add("bg-blue-400");
+        button_get.classList.remove("bg-blue-500");
+    }
+);
+
+button_get.addEventListener(
+    "mouseup",
+    () => {
+        button_get.classList.add("bg-blue-500");
+        button_get.classList.remove("bg-blue-400");
+    }
+);
+
+button_get.addEventListener(
+    "mouseleave",
+    () => {
+        button_get.classList.add("bg-blue-500");
+        button_get.classList.remove("bg-blue-400");
+    }
+);
+
+
+button_clear.addEventListener(
+    "mousedown",
+    () => {
+        button_clear.classList.add("bg-rose-400");
+        button_clear.classList.remove("bg-rose-500");
+    }
+);
+
+button_clear.addEventListener(
+    "mouseup",
+    () => {
+        button_clear.classList.add("bg-rose-500");
+        button_clear.classList.remove("bg-rose-400");
+    }
+);
+
+button_clear.addEventListener(
+    "mouseleave",
+    () => {
+        button_clear.classList.add("bg-rose-500");
+        button_clear.classList.remove("bg-rose-400");
+    }
+);
