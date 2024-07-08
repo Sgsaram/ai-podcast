e0179c28ab4e578e0649771acb1386055f5ed3f9
john <john>
2024-07-08 17:44:25 +0300

    Added tags

--- static/script.js
+++ static/script.js
@@ -7,10 +7,36 @@ const link = document.getElementById("link");
 const required = document.getElementById("required");
 const views = document.getElementById("views");
 const recommendations = document.getElementById("recommendations");
+const new_tag = document.getElementById("new-tag");
+const tags = document.getElementById("tags");
 
 const api_url = "http://127.0.0.1:8000/process"
 
 let bad_desc = false;
+let tags_list = [];
+
+function get_html_tags() {
+    res = "";
+    for (let tag of tags_list) {
+        res += '<span class="rounded-2xl bg-violet-500 p-1 px-5 mx-1 my-1"><span class="text-white font-semibold text-sm">' + tag + '</span></span>';
+    }
+    return res;
+}
+
+function update_tags() {
+    tags.innerHTML = get_html_tags();
+}
+
+function remove_tag(value) {
+    const index = tags_list.indexOf(value);
+    if (index != -1) {
+        tags_list.splice(index, 1);
+    }
+}
+
+function add_tag(value) {
+    tags_list.push(value);
+}
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -134,3 +160,16 @@ button_clear.addEventListener(
         button_clear.classList.remove("bg-rose-400");
     }
 );
+
+
+new_tag.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        if (new_tag.value.trim() != "") {
+            add_tag(new_tag.value.trim());
+            update_tags();
+            new_tag.value = "";
+        }
+    }
+});
+
